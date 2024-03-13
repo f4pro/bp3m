a2eaf582d7b3a82b7a7974b6884dafef47e7883b
Muhammad Faizil Putra <faizilputra7@gmail.com>
2024-03-13 12:58:40 +0700

    First commit

---
+++ application/views/pub/vw_sertifikat5.php
@@ -0,0 +1,38 @@
+<div id="hero" class="hero overlay subpage-hero sertifikasi-hero">
+        <div class="hero-content container">
+            <div class="hero-text row">
+                <div class="col">
+                    <h1>Sertifikasi</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div class="container">
+    <h1>DataTable Example</h1>
+    <table id="tabel_ser" class="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John Doe</td>
+          <td>30</td>
+          <td>New York</td>
+        </tr>
+        <tr>
+          <td>Jane Smith</td>
+          <td>25</td>
+          <td>Los Angeles</td>
+        </tr>
+        <!-- Add more rows here -->
+    </tbody>
+  </table>
+</div>
+
+
+    
